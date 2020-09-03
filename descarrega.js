89c3738d8773e3a754c85658a08e7c0609ad1def
João Maranhão <joaofelipemaranhao@gmail.com>
2020-09-03 17:29:24 -0300

    refactor: organize the file

--- descarrega.js
+++ descarrega.js
@@ -1,53 +1,69 @@
 var fs = require('fs')
 var readline = require('readline-sync')
 
+
 const date = new Date()
 
-// PEGANDO A DATA DO DIA
-const content = {
+const todayDate = {
   year: date.getFullYear(),
   month: date.getMonth() + 1 > 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`,
   day: date.getDate() > 10 ? date.getDate() : `0${date.getDate()}`,
   monthsArray: ['JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO'],
-  fullMonth: '',
-  fileName: '',
-  files: fs.readdirSync(`G:\\\\Clip`).filter(fn => fn.endsWith('.MXF')),
-
 }
 
-content.fullMonth = content.monthsArray[date.getMonth()]
-content.fileName = `${content.year}${content.month}${content.day}`
+const folderName = {
+  fullMonth: todayDate.monthsArray[date.getMonth()],
+  folderDate: `${todayDate.year}${todayDate.month}${todayDate.day}`,
+}
 
+const paths = {
+  deckPath: `G:\\\\Clip`,
+  bkpPath: `Q:`,
+  fullBkpPath: ''
+}
 
-// PEGANDO A RETRANCA
+const filesPath = {
+  files: fs.readdirSync(paths.deckPath).filter(fn => fn.endsWith('.MXF')),
+}
+
 const retranca = readline.question("Escreva a retranca: ")
 
+
 // FUNÇÕES
+const createYearFolder = () => {
+  const yearFolder = `${paths.bkpPath}\\\\${todayDate.year}`
+  fs.existsSync(yearFolder) ? true : fs.mkdirSync(yearFolder)
+}
+
 const createMonthFolder = () => {
-  fs.existsSync(`Q:\\\\2020\\\\${content.fullMonth}`) ? true : fs.mkdirSync(`Q:\\\\2020\\\\${content.fullMonth}`)
+  const monthFolder = `${paths.bkpPath}\\\\${todayDate.year}\\\\${folderName.fullMonth}`
+  fs.existsSync(monthFolder) ? true : fs.mkdirSync(monthFolder)
 }
 
 const createDayFolder = () => {
-  fs.existsSync(`Q:\\\\2020\\\\${content.fullMonth}\\\\${content.day}`) ? true : fs.mkdirSync(`Q:\\\\2020\\\\${content.fullMonth}\\\\${content.day}`)
+  const dayFolder = `${paths.bkpPath}\\\\${todayDate.year}\\\\${folderName.fullMonth}\\\\${todayDate.day}`
+  fs.existsSync(dayFolder) ? true : fs.mkdirSync(dayFolder)
 }
 
 const createScheduleFolder = () => {
-  fs.existsSync(`Q:\\\\2020\\\\${content.fullMonth}\\\\${content.day}\\\\${content.fileName}_${retranca.toUpperCase()}`) ? true : fs.mkdirSync(`Q:\\\\2020\\\\${content.fullMonth}\\\\${content.day}\\\\${content.fileName}_${retranca.toUpperCase()}`)
+  const fullBkpPath = `${paths.bkpPath}\\\\${todayDate.year}\\\\${folderName.fullMonth}\\\\${todayDate.day}\\\\${folderName.folderDate}_${retranca.toUpperCase()}`
+  fs.existsSync(fullBkpPath) ? true : fs.mkdirSync(fullBkpPath)
+  paths.fullBkpPath = fullBkpPath
+}
+
+const copyRawImages = () => {
+  fs.existsSync(`${paths.fullBkpPath}\\\\C0001.MXF`) ? console.log('Já ta na pasta') : copyFile()
 }
 
 const copyFile = () => {
   console.log('Transferindo...')
-  content.files.map((file) => {
-    fs.copyFileSync(`G:\\\\Clip\\\\${file}`, `Q:\\\\2020\\\\${content.fullMonth}\\\\${content.day}\\\\${content.fileName}_${retranca.toUpperCase()}\\\\${file}`)
+  filesPath.files.map((file) => {
+    fs.copyFileSync(`${paths.deckPath}\\\\${file}`, `${paths.fullBkpPath}\\\\${file}`)
   })
   console.log('Terminou!')
 }
 
-const copyRawImages = () => {
-  fs.existsSync(`Q:\\\\2020\\\\${content.fullMonth}\\\\${content.day}\\\\${retranca.toUpperCase()}\\\\C0001.MXF`) ? console.log('Já ta na pasta') : copyFile()
-  
-}
-
+createYearFolder()
 createMonthFolder()
 createDayFolder()
 createScheduleFolder()
